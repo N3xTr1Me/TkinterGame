6508bce2059c7b3e95fdc2231a376a19faafba5e
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2019-08-19 15:16:09 +0300

    Player n exit creation

--- Game.py
+++ Game.py
@@ -1,15 +1,44 @@
 import tkinter as tk
 import random as rn
 
-def randomPoz(top):
-    return rn.randint(1, top - 1)*step
+class Player():
+
+    def __init__(this, clr):
+        this.x = this.randomPoz(N_X)
+        this.y = this.randomPoz(N_Y)
+        this.color = clr
+
+    def draw(this):
+        canvas.create_oval((this.x, this.y),
+                           (this.x+step, this.y+step),
+                            fill = this.color)
+        
+    def randomPoz(this, top):
+        return rn.randint(1, top - 1)*step
 
 master = tk.Tk()
 step = 60
 N_X = 10
 N_Y = 10
-canvas = tk.Canvas(master, bg = 'blue', height = step*N_X, width = step*N_Y)
+canvas = tk.Canvas(master, bg = '#04F8D3', height = step*N_X, width = step*N_Y)
 
-player_pos = (randomPoz(N_X), randomPoz(N_Y))
-exit_pos   = (randomPoz(N_X), randomPoz(N_Y))
+player = Player('#04EFF6')
+player.draw()
+exit_g = Player('#F6DA08')
+exit_g.draw()
+
+
+
+canvas.pack()
+master.mainloop()
+
+
+
+
+
+
+
+
+
+
 

eb4e7095e0ffaf045716440a33f5bdf898034f1d
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2019-08-19 14:52:56 +0300

    add PozGeneration

--- Game.py
+++ Game.py
@@ -1,1 +1,15 @@
-print('Hello world!')
+import tkinter as tk
+import random as rn
+
+def randomPoz(top):
+    return rn.randint(1, top - 1)*step
+
+master = tk.Tk()
+step = 60
+N_X = 10
+N_Y = 10
+canvas = tk.Canvas(master, bg = 'blue', height = step*N_X, width = step*N_Y)
+
+player_pos = (randomPoz(N_X), randomPoz(N_Y))
+exit_pos   = (randomPoz(N_X), randomPoz(N_Y))
+

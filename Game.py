17c8bbcf60e404f56c4136bff1b63795b9a221d3
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2019-08-21 14:47:00 +0300

    add GameOver func

--- Game.py
+++ Game.py
@@ -2,14 +2,29 @@ import tkinter as tk
 import random as rn
 
 def k_prsd(event):
-    if event.keysym == 'Up':
-        canvas.move(pl, 0, -(step))
-    if event.keysym == 'Down':
-        canvas.move(pl, 0, step)
-    if event.keysym == 'Left':
-        canvas.move(pl, -(step), 0)
-    if event.keysym == 'Right':
-        canvas.move(pl, step, 0)
+    if event.keysym == 'Up' or event.keysym == 'w':
+        player.y -= step
+        reDraw(pl, 0, -(step))
+    if event.keysym == 'Down' or event.keysym == 's':
+        player.y += step
+        reDraw(pl, 0, step)
+    if event.keysym == 'Left' or event.keysym == 'a':
+        player.x -= step
+        reDraw(pl, -(step), 0)
+    if event.keysym == 'Right' or event.keysym == 'd':
+        player.x += step
+        reDraw(pl, step, 0)
+    endGame()
+
+def reDraw(obj, x, y):
+    canvas.move(obj, x, y)
+
+def endGame():
+    print('player:', player.x, player.y)
+    print('exit_g:', exit_g.x, exit_g.y)
+    if player.comparePoz(exit_g):
+        print('GG')
+        print('U won!!!')
 
 class Player():
 
@@ -19,13 +34,17 @@ class Player():
         this.color = clr
 
     def draw(this):
-        a = canvas.create_oval((this.x, this.y),
-                           (this.x+step, this.y+step),
-                            fill = this.color)
-        return a
+        body = canvas.create_oval((this.x, this.y),
+                                  (this.x+step, this.y+step),
+                                   fill = this.color)
+        return body
         
     def randomPoz(this, top):
         return rn.randint(1, top - 1)*step
+
+    def comparePoz(this, other):
+        return this.x == other.x and this.y == other.y
+
 
 master = tk.Tk()
 step = 60

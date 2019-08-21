8e4d0702415c98137845597812f3f6756aa8dd66
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2019-08-21 08:06:29 +0300

    added movement

--- Game.py
+++ Game.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 import random as rn
 
+def k_prsd(event):
+    if event.keysym == 'Up':
+        canvas.move(pl, 0, -(step))
+    if event.keysym == 'Down':
+        canvas.move(pl, 0, step)
+    if event.keysym == 'Left':
+        canvas.move(pl, -(step), 0)
+    if event.keysym == 'Right':
+        canvas.move(pl, step, 0)
+
 class Player():
 
     def __init__(this, clr):
@@ -9,9 +19,10 @@ class Player():
         this.color = clr
 
     def draw(this):
-        canvas.create_oval((this.x, this.y),
+        a = canvas.create_oval((this.x, this.y),
                            (this.x+step, this.y+step),
                             fill = this.color)
+        return a
         
     def randomPoz(this, top):
         return rn.randint(1, top - 1)*step
@@ -20,20 +31,16 @@ master = tk.Tk()
 step = 60
 N_X = 10
 N_Y = 10
-canvas = tk.Canvas(master, bg = '#04F8D3', height = step*N_X, width = step*N_Y)
+canvas = tk.Canvas(master, bg = '#00FFFF', height = step*N_X, width = step*N_Y)
 
-player = Player('#04EFF6')
-player.draw()
-exit_g = Player('#F6DA08')
+player = Player('#32CD32')
+pl = (player.draw())
+exit_g = Player('#FFD700')
 exit_g.draw()
 
-
-
 canvas.pack()
+master.bind('<KeyPress>', k_prsd)
 master.mainloop()
-
-
-
 
 
 
